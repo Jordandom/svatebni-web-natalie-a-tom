376ba690bd37d0209fa4a60a9c20e050a28dc5b5
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2024-07-11 13:42:02 +0200

    centered hero

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 export const Hero = () => (
   <Image
+    className="mx-auto my-0"
     priority
     src="/uvodni-obrazek-transparent-compress.webp"
     width={1920}
